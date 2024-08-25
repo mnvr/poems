30c3f7657c38ef3949fde1d341d5a9a2d5f9d28b
Manav Rathi <manav@mrmr.io>
2024-08-25 19:38:06 +0530

    Feed
    Ref: https://validator.w3.org/feed/docs/atom.html

--- .vitepress/feed.ts
+++ .vitepress/feed.ts
@@ -1,5 +1,46 @@
-import type { SiteConfig } from "vitepress";
+import { createContentLoader, type SiteConfig } from "vitepress";
 
-export const generateFeed = (siteConfig: SiteConfig) => {
-    console.log(siteConfig);
-}
+const baseURL = "https://poems.mrmr.io";
+
+export const generateFeed = async (siteConfig: SiteConfig) => {
+    const pages = await createContentLoader("*.md").load();
+    const pageData = pages
+        .map(({ url, frontmatter }) => {
+            const { title, date } = frontmatter;
+            // Ignore pages that don't have a frontmatter date (e.g. /random).
+            if (!title || !date) return undefined;
+            // `url` is the slug.
+            const href = baseURL + url;
+            return { title, date, href };
+        })
+        .filter((s) => !!s)
+        // Descending order, most recent first
+        .sort((a, b) => b.date - a.date);
+
+    const entries = pageData
+        .map(
+            ({ title, date, href }) =>
+                `  <entry>
+    <title>${title}</title>
+    <link href="${href}"/>
+    <id>${href}</id>
+    <updated>${date.toISOString()}</updated>
+  </entry>`
+        )
+        .join("\n");
+
+    const updated = pageData[0]!.date;
+    const feed = `<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <title>Poems and other fiction by Manav</title>
+  <id>${baseURL}</id>
+  <link href="${baseURL}"/>
+  <link rel="self" href="/feed.xml"/>
+  <updated>${updated.toISOString()}</updated>
+  <author><name>Manav Rathi</name></author>
+${entries}
+</feed>
+`;
+
+    console.log(feed);
+};
